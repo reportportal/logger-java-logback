3485d607d2b512088b7f1a85b4624540af1a33d7
pavel_bortnik <pavel_bortnik@epam.com>
2019-07-02 18:06:36 +0300

    remove pattern from appender

--- src/main/java/com/epam/reportportal/logback/appender/ReportPortalAppender.java
+++ src/main/java/com/epam/reportportal/logback/appender/ReportPortalAppender.java
@@ -20,7 +20,6 @@
  */
 package com.epam.reportportal.logback.appender;
 
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import com.epam.reportportal.message.HashMarkSeparatedMessageParser;
@@ -40,62 +39,43 @@ import static com.epam.reportportal.service.ReportPortal.emitLog;
  */
 public class ReportPortalAppender extends AppenderBase<ILoggingEvent> {
 
-    private static final MessageParser MESSAGE_PARSER = new HashMarkSeparatedMessageParser();
-    private PatternLayoutEncoder encoder;
+	private static final MessageParser MESSAGE_PARSER = new HashMarkSeparatedMessageParser();
 
-    @Override
-    protected void append(final ILoggingEvent event) {
-        emitLog(new Function<String, com.epam.ta.reportportal.ws.model.log.SaveLogRQ>() {
-            @Override
-            public com.epam.ta.reportportal.ws.model.log.SaveLogRQ apply(String itemId) {
-                final String message = event.getFormattedMessage();
-                final String level = event.getLevel().toString();
-                final Date time = new Date(event.getTimeStamp());
+	@Override
+	protected void append(final ILoggingEvent event) {
+		emitLog(new Function<String, com.epam.ta.reportportal.ws.model.log.SaveLogRQ>() {
+			@Override
+			public com.epam.ta.reportportal.ws.model.log.SaveLogRQ apply(String itemId) {
+				final String message = event.getFormattedMessage();
+				final String level = event.getLevel().toString();
+				final Date time = new Date(event.getTimeStamp());
 
-                SaveLogRQ rq = new SaveLogRQ();
-                rq.setLevel(level);
-                rq.setLogTime(time);
-                rq.setItemId(itemId);
+				SaveLogRQ rq = new SaveLogRQ();
+				rq.setLevel(level);
+				rq.setLogTime(time);
+				rq.setItemId(itemId);
 
-                try {
-                    if (MESSAGE_PARSER.supports(message)) {
-                        ReportPortalMessage rpMessage = MESSAGE_PARSER.parse(message);
-                        TypeAwareByteSource data = rpMessage.getData();
-                        com.epam.ta.reportportal.ws.model.log.SaveLogRQ.File file = new com.epam.ta.reportportal.ws.model.log.SaveLogRQ.File();
-                        file.setContent(data.read());
-                        file.setContentType(data.getMediaType());
-                        file.setName(UUID.randomUUID().toString());
+				try {
+					if (MESSAGE_PARSER.supports(message)) {
+						ReportPortalMessage rpMessage = MESSAGE_PARSER.parse(message);
+						TypeAwareByteSource data = rpMessage.getData();
+						com.epam.ta.reportportal.ws.model.log.SaveLogRQ.File file = new com.epam.ta.reportportal.ws.model.log.SaveLogRQ.File();
+						file.setContent(data.read());
+						file.setContentType(data.getMediaType());
+						file.setName(UUID.randomUUID().toString());
 
-                        rq.setFile(file);
-                        rq.setMessage(rpMessage.getMessage());
-                    } else {
-                        rq.setMessage(encoder.getLayout().doLayout(event));
-                    }
+						rq.setFile(file);
+						rq.setMessage(rpMessage.getMessage());
+					} else {
+						rq.setMessage(message);
+					}
 
-                } catch (Exception e) {
-                    //skip
-                }
+				} catch (Exception e) {
+					//skip
+				}
 
-                return rq;
-            }
-        });
-    }
-
-    @Override
-    public void start() {
-        if (this.encoder == null) {
-            addError("No encoder set for the appender named [" + name + "].");
-            return;
-        }
-        this.encoder.start();
-        super.start();
-    }
-
-    public PatternLayoutEncoder getEncoder() {
-        return encoder;
-    }
-
-    public void setEncoder(PatternLayoutEncoder encoder) {
-        this.encoder = encoder;
-    }
+				return rq;
+			}
+		});
+	}
 }
