a2569f100ec5576c150dff0a7b4d43d02ec8cc2c
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-09-26 12:01:11 +0300

    Client version update

--- src/main/java/com/epam/reportportal/logback/appender/ReportPortalAppender.java
+++ src/main/java/com/epam/reportportal/logback/appender/ReportPortalAppender.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2019 EPAM Systems
+ * Copyright 2025 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.epam.reportportal.logback.appender;
 
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
@@ -24,8 +25,8 @@ import com.epam.reportportal.message.ReportPortalMessage;
 import com.epam.reportportal.message.TypeAwareByteSource;
 import com.epam.ta.reportportal.ws.model.log.SaveLogRQ;
 
+import java.time.Instant;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,67 +37,66 @@ import static com.epam.reportportal.service.ReportPortal.emitLog;
  */
 public class ReportPortalAppender extends AppenderBase<ILoggingEvent> {
 
-    private static final MessageParser MESSAGE_PARSER = new HashMarkSeparatedMessageParser();
-    private static final List<String> LOGGING_ISSUE =
-            Collections.singletonList("com.epam.reportportal.service.logs.LoggingSubscriber");
-    private PatternLayoutEncoder encoder;
+	private static final MessageParser MESSAGE_PARSER = new HashMarkSeparatedMessageParser();
+	private static final List<String> LOGGING_ISSUE = Collections.singletonList("com.epam.reportportal.service.logs.LoggingSubscriber");
+	private PatternLayoutEncoder encoder;
 
-    @Override
-    protected void append(final ILoggingEvent event) {
-        String loggerName = event.getLoggerName();
-        for (String packagePrefix : LOGGING_ISSUE) {
-            if (loggerName.startsWith(packagePrefix)) {
-                return;
-            }
-        }
+	@Override
+	protected void append(final ILoggingEvent event) {
+		String loggerName = event.getLoggerName();
+		for (String packagePrefix : LOGGING_ISSUE) {
+			if (loggerName.startsWith(packagePrefix)) {
+				return;
+			}
+		}
 
-        emitLog(itemUuid -> {
-            final String message = event.getFormattedMessage();
-            final String level = event.getLevel().toString();
-            final Date time = new Date(event.getTimeStamp());
+		emitLog(itemUuid -> {
+			final String message = event.getFormattedMessage();
+			final String level = event.getLevel().toString();
+			final Instant time = event.getInstant();
 
-            SaveLogRQ request = new SaveLogRQ();
-            request.setLevel(level);
-            request.setLogTime(time);
-            request.setItemUuid(itemUuid);
+			SaveLogRQ request = new SaveLogRQ();
+			request.setLevel(level);
+			request.setLogTime(time);
+			request.setItemUuid(itemUuid);
 
-            try {
-                if (MESSAGE_PARSER.supports(message)) {
-                    ReportPortalMessage rpMessage = MESSAGE_PARSER.parse(message);
-                    TypeAwareByteSource data = rpMessage.getData();
-                    SaveLogRQ.File file = new SaveLogRQ.File();
-                    file.setContent(data.read());
-                    file.setContentType(data.getMediaType());
-                    file.setName(UUID.randomUUID().toString());
+			try {
+				if (MESSAGE_PARSER.supports(message)) {
+					ReportPortalMessage rpMessage = MESSAGE_PARSER.parse(message);
+					TypeAwareByteSource data = rpMessage.getData();
+					SaveLogRQ.File file = new SaveLogRQ.File();
+					file.setContent(data.read());
+					file.setContentType(data.getMediaType());
+					file.setName(UUID.randomUUID().toString());
 
-                    request.setFile(file);
-                    request.setMessage(rpMessage.getMessage());
-                } else {
-                    request.setMessage(encoder.getLayout().doLayout(event));
-                }
-            } catch (Exception e) {
-                //skip
-            }
-            return request;
-        });
-    }
+					request.setFile(file);
+					request.setMessage(rpMessage.getMessage());
+				} else {
+					request.setMessage(encoder.getLayout().doLayout(event));
+				}
+			} catch (Exception e) {
+				//skip
+			}
+			return request;
+		});
+	}
 
-    @Override
-    public void start() {
-        if (this.encoder == null) {
-            addError("No encoder set for the appender named [" + name + "].");
-            return;
-        }
-        this.encoder.start();
-        super.start();
-    }
+	@Override
+	public void start() {
+		if (this.encoder == null) {
+			addError("No encoder set for the appender named [" + name + "].");
+			return;
+		}
+		this.encoder.start();
+		super.start();
+	}
 
-    @SuppressWarnings("unused")
-    public PatternLayoutEncoder getEncoder() {
-        return encoder;
-    }
+	@SuppressWarnings("unused")
+	public PatternLayoutEncoder getEncoder() {
+		return encoder;
+	}
 
-    public void setEncoder(PatternLayoutEncoder encoder) {
-        this.encoder = encoder;
-    }
+	public void setEncoder(PatternLayoutEncoder encoder) {
+		this.encoder = encoder;
+	}
 }
